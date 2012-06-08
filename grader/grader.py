01a0cd6cdb117c967f4476a3e2f56b5b20042a9f
Luis Naranjo <luisnaranjo733@hotmail.com>
2012-06-07 19:07:07 -0700

    Renames all to lowercase now - consistently

--- grader/grader.py
+++ grader/grader.py
@@ -2,11 +2,12 @@ import os
 import sys
 import shutil
 import logging
+from dircache import listdir
 from datetime import datetime
 
 from configobj import ConfigObj
 
-DEBUG = False
+DEBUG = True
 show_members = True
 
 home = os.path.expanduser('~')
@@ -29,7 +30,7 @@ if not os.path.isdir(project_root):
 settings_path = os.path.join(project_root,'settings.conf')
 students_path = os.path.join(project_root, 'students.txt')
 log_path = os.path.join(project_root, 'log.txt')
-grader_path = os.path.join(project_root, 'grader_files')
+grader_path = os.path.abspath(os.path.join(project_root, 'grader_files'))
 
 for filepath in [settings_path, students_path, log_path]:
     try:
@@ -41,7 +42,7 @@ if not DEBUG:
     logging.basicConfig(format='%(message)s', filename=log_path)
 
 if DEBUG:
-    logging.basicConfig(format='%(levelname)s:%(message)s')
+    logging.basicConfig(format='%(message)s')
 
 config = ConfigObj(settings_path)
 #{'project 3': {'exercise 1': ['.jpg', '.3dm'], 'exercise 2': '.3dm'}}
@@ -52,17 +53,32 @@ students = list()
 with open(students_path) as fh:
     students = [line.strip() for line in fh.readlines()]
 
-date = datetime.now().strftime('%B %d %Y %I:%M %P')
-entry_title = "log entry on %r" % date
+if not DEBUG:
+    date = datetime.now().strftime('%B %d %Y %I:%M %P')
+    entry_title = "log entry on %r" % date
+    logging.warning(entry_title.center(70))
 
-logging.warning(entry_title.center(70))
+
+def rename_all( root, items):
+    for name in items:
+        try:
+            os.rename( os.path.join(root, name), 
+                                os.path.join(root, name.lower()))
+        except OSError:
+            pass # can't rename it, so what
+
+    # starts from the bottom so paths further up remain valid after renaming
+for root, dirs, files in os.walk(grader_path, topdown=False ):
+    rename_all( root, dirs )
+    rename_all( root, files)
+
 
 for project in config:
-    project = project.lower()
     for lastname in students:
         lastname = lastname.lower()
         exercises = config[project]  # {'exercise 1': ['.jpg', '.3dm'], 'exercise 2': '.3dm'}
         expected_foldername = '%s %s' % (lastname, project)
+        #expected_foldername = expected_foldername.capitalize()
         expected_folder = os.path.join(grader_path, expected_foldername)
         if not os.path.isdir(expected_folder):
             warning = "%s is missing %s folder" % (lastname, project)
@@ -77,13 +93,14 @@ for project in config:
             assert isinstance(extensions, list)
 
             for ext in extensions:
-                expected_exercise_name = '%s %s%s' % (lastname, exercise, ext)
+                expected_exercise_name = '%s %s' % (lastname, exercise)
+                expected_exercise_name = expected_exercise_name.lower() + ext  # TODO: FINALIZE
 
                 expected_exercise = os.path.join(expected_folder, expected_exercise_name)
                 if not os.path.isfile(expected_exercise):
                     logging.warning('%s is missing %s:%s' % (lastname, project, exercise+ext))
 
-logging.warning('')
+if not DEBUG: logging.warning('')
 
 
 
